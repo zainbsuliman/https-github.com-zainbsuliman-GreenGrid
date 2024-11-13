df3625e0b97143ab46a0461bec731ce44669ff73
zainbsuliman <zat301099@gmail.com>
2024-11-13 13:01:29 +0300

    Initial Commit

---
+++ greengrid/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  greengrid
+//
+//  Created by Zainab Alatwi  on 11/05/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ greengrid/greengridApp.swift
@@ -0,0 +1,17 @@
+//
+//  greengridApp.swift
+//  greengrid
+//
+//  Created by Zainab Alatwi  on 11/05/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct greengridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
